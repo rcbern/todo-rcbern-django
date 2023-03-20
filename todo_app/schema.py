18daa53158a50103887255f243bde975eda3564c
rcbern <rbcon.ks77m@slmail.me>
2023-03-20 12:35:59 +0800

    api views

--- todo_app/schema.py
+++ todo_app/schema.py
@@ -1,35 +1,46 @@
-from pydantic import Field
+from pydantic import Field, validator
 from ninja import Schema
-from datetime import date
+from datetime import datetime
+
+
+class Message(Schema):
+    message: str
 
 
 class TaskIn(Schema):
     task_name: str = Field(..., description="Task Name")
-    task_description: str | None = Field(None, description="Task Description")
-    task_category: str | None = Field(None, description="Task Category")
+    task_description: str = Field(description="Task Description")
+    task_category: str = Field(description="Task Category")
     is_completed: bool | None = Field(None, description="Task Status")
-    date_completion: date | None = Field(None, description="Task Completion")
-    date_creation: date | None = Field(None, description="Task Creation")
+    date_completion: datetime | None = Field(None, description="Task Completion")
+    date_creation: datetime | None = Field(None, description="Task Creation")
 
+    @validator('task_description')
+    def validate_desc(cls, value):
+        if len(value) > 50:
+            raise ValueError(f"Task description should be less than 50 characters.")
+        return value
 
 class TaskPut(Schema):
     task_name: str | None = Field(None, description="Task Name")
     task_description: str | None = Field(None, description="Task Description")
     task_category: str | None = Field(None, description="Task Category")
     is_completed: bool | None = Field(None, description="Task Status")
-    date_completion: date | None = Field(None, description="Task Completion")
-    date_creation: date | None = Field(None, description="Task Creation")
+    date_completion: datetime | None = Field(None, description="Task Completion")
+    date_creation: datetime | None = Field(None, description="Task Creation")
+
+    @validator('task_description')
+    def validate_desc(cls, value):
+        if len(value) > 50:
+            raise ValueError(f"Task description should be less than 50 characters.")
+        return value
 
 
 class TaskOut(Schema):
     id: int
     task_name: str | None = Field(None, description="Task Name")
     task_description: str | None = Field(None, description="Task Description")
-    task_category: str | None = Field(None, description="Task Category")
-    # is_completed: bool | None = Field(None, description="Task Status")
-    # date_completion: date | None = Field(None, description="Task Completion")
-    # date_creation: date | None = Field(None, description="Task Creation")
-
-
-class Message(Schema):
-    message: str
+    # task_category: str | None = Field(None, description="Task Category")
+    is_completed: bool | None = Field(None, description="Task Status")
+    # date_completion: datetime | None = Field(None, description="Task Completion")
+    # date_creation: datetime | None = Field(None, description="Task Creation")
